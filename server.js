d0546ae6eedc0f02e597d239d30a4b72baf2dca5
benjaminpettus <benjaminpettus@gmail.com>
2017-10-31 11:39:09 -0700

    added bulma

--- server.js
+++ server.js
@@ -4,12 +4,17 @@ const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const path = require('path')
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'views')))
-
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.use('/bulma', express.static('node_modules/bulma/css'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+
+
+
 
 app.get('/', (request, response) => {
   response.render('index')
