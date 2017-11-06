4988aa1dbab85aa3e060de52a4bc70deda0c20db
benjaminpettus <benjaminpettus@gmail.com>
2017-11-06 12:42:26 -0800

    added city to navbar and formatted post page WIP edit profile

--- routes/post.js
+++ routes/post.js
@@ -4,6 +4,7 @@ const User = require('../db/user')
 
 post.get('/:id', ( request, response ) => {
   const { id } = request.params
+  console.log('session from route::::', request.session)
   Posts.byId( id )
   .then( post => {
     response.render('post', {post:post, session: request.session})
